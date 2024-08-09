4db4e5626437be3aec381c3201e3198d78302e54
Chris Roach <c.roach10@yahoo.com>
2024-08-08 20:36:04 -0400

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class TaskController extends Controller
+{
+    /**
+     * Display the user's profile form.
+     */
+    public function edit(Request $request): Response
+    {
+        return Inertia::render('Profile/Edit', [
+            'mustVerifyEmail' => $request->user() instanceof MustVerifyEmail,
+            'status' => session('status'),
+        ]);
+    }
+
+    /**
+     * Handle an incoming new password request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+        
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user) use ($request) {
+                $user->forceFill([
+                    'password' => Hash::make($request->password),
+                    'remember_token' => Str::random(60),
+                ])->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        // If the password was successfully reset, we will redirect the user back to
+        // the application's home authenticated view. If there is an error we can
+        // redirect them back to where they came from with their error message.
+        if ($status == Password::PASSWORD_RESET) {
+            return redirect()->route('login')->with('status', __($status));
+        }
+
+        throw ValidationException::withMessages([
+            'email' => [trans($status)],
+        ]);
+    }
+
+    /**
+     * Update the user's profile information.
+     */
+    public function update(ProfileUpdateRequest $request): RedirectResponse
+    {
+        $request->user()->fill($request->validated());
+
+        if ($request->user()->isDirty('email')) {
+            $request->user()->email_verified_at = null;
+        }
+
+        $request->user()->save();
+
+        return Redirect::route('profile.edit');
+    }
+
+    /**
+     * Delete the user's account.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'password' => ['required', 'current_password'],
+        ]);
+
+        $user = $request->user();
+
+        Auth::logout();
+
+        $user->delete();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return Redirect::to('/');
+    }
+}
---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,146 @@
+<script setup lang="ts">
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import { Activity, ArrowUpRight, CreditCard, DollarSign, Users } from 'lucide-vue-next'
+import { Button } from '@/Components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/Components/ui/card'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/Components/ui/table'
+
+</script>
+
+
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+
+        <template #header>
+            <h1>Dashboard</h1>
+        </template>
+
+        <div class="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+            <Card>
+                <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle class="text-sm font-medium">
+                        Total Revenue
+                    </CardTitle>
+                    <DollarSign class="h-4 w-4 text-muted-foreground" />
+                </CardHeader>
+                <CardContent>
+                    <div class="text-2xl font-bold">
+                        $45,231.89
+                    </div>
+                    <p class="text-xs text-muted-foreground">
+                        +20.1% from last month
+                    </p>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle class="text-sm font-medium">
+                        Subscriptions
+                    </CardTitle>
+                    <Users class="h-4 w-4 text-muted-foreground" />
+                </CardHeader>
+                <CardContent>
+                    <div class="text-2xl font-bold">
+                        +2350
+                    </div>
+                    <p class="text-xs text-muted-foreground">
+                        +180.1% from last month
+                    </p>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle class="text-sm font-medium">
+                        Sales
+                    </CardTitle>
+                    <CreditCard class="h-4 w-4 text-muted-foreground" />
+                </CardHeader>
+                <CardContent>
+                    <div class="text-2xl font-bold">
+                        +12,234
+                    </div>
+                    <p class="text-xs text-muted-foreground">
+                        +19% from last month
+                    </p>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle class="text-sm font-medium">
+                        Active Now
+                    </CardTitle>
+                    <Activity class="h-4 w-4 text-muted-foreground" />
+                </CardHeader>
+                <CardContent>
+                    <div class="text-2xl font-bold">
+                        +573
+                    </div>
+                    <p class="text-xs text-muted-foreground">
+                        +201 since last hour
+                    </p>
+                </CardContent>
+            </Card>
+        </div>
+        <div class="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
+            <Card class="xl:col-span-2">
+                <CardHeader class="flex flex-row items-center">
+                    <div class="grid gap-2">
+                        <CardTitle>Tasks</CardTitle>
+                        <CardDescription>
+                            Tasks to crush.
+                        </CardDescription>
+                    </div>
+                    <Button as-child size="sm" class="ml-auto gap-1">
+                        <a href="#">
+                            View All
+                            <ArrowUpRight class="h-4 w-4" />
+                        </a>
+                    </Button>
+                </CardHeader>
+                <CardContent>
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>Task</TableHead>
+                                <TableHead class="hidden xl:table-column">
+                                    Type
+                                </TableHead>
+                                <TableHead class="text-right">
+                                    Crush it
+                                </TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>
+                                    <div class="font-medium">
+                                        Liam Johnson
+                                    </div>
+                                    <div class="hidden text-sm text-muted-foreground md:inline">
+                                        liam@example.com
+                                    </div>
+                                </TableCell>
+                                <TableCell class="text-right">
+                                    $250.00
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Recent Notes</CardTitle>
+                </CardHeader>
+                <CardContent class="grid gap-8">
+
+                </CardContent>
+            </Card>
+        </div>
+
+    </AuthenticatedLayout>
+</template>
